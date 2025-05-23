3426c3685a2da877ad0958f015154c1ae8b16ccd
Maximiliano Martinez <maxi.martinezs@duocuc.cl>
2025-05-23 02:29:51 -0400

    Resto del programa

---
+++ src/main/java/com/tienda_fs/tienda_fs/TiendaFsApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_fs.tienda_fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda_fs/tienda_fs/TiendaFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_fs.tienda_fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
