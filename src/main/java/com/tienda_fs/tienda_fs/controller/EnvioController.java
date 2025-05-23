3e1ae894af51b4169bd4bd9b9c0800348587964f
Maximiliano Martinez <maxi.martinezs@duocuc.cl>
2025-05-23 16:20:58 -0400

    commit final

--- src/main/java/com/tienda_fs/tienda_fs/controller/EnvioController.java
+++ src/main/java/com/tienda_fs/tienda_fs/controller/EnvioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/envios")
+@RequestMapping("/api/envios")
 public class EnvioController {
 
     private final EnvioService envioService;
